f579f9d675a61cd35212bd17c36cd6ae51897326
INNMukul <mukul.shivam@innoverdigital.com>
2025-03-19 23:29:13 +0530

    Refactor pyramid function to generate various half pyramid patterns with letters and asterisks

--- halfPyramid.py
+++ halfPyramid.py
@@ -26,3 +26,80 @@ x = int(input())
 
 pyramid(x)
 #=============================================================================================
+
+def pyramid(x):
+    
+    a = 65
+    for i in range(1, x + 1):
+     #   spaces = " " * (x - i)
+        stars = "*" * (i )
+        print(stars)
+        a += 1
+
+x = int(input())
+
+pyramid(x)
+
+#=============================================================================================
+
+
+def pyramid(x):
+    
+    a = 65
+    for i in range(1, x + 1):
+     #   spaces = " " * (x - i)
+        stars = "%c" %(a) * (i )
+        print(stars)
+        a += 1
+
+x = int(input())
+
+pyramid(x)
+
+#=============================================================================================
+
+def pyramid(x):
+    
+    a = 65
+    for i in range(1, x + 1):
+     #   spaces = " " * (x - i)
+        stars = "%c" %(a) * (i -1)
+        print(stars)
+        a += 1
+
+x = int(input())
+
+pyramid(x)
+
+#=============================================================================================
+
+def pyramid(x):
+    
+    a = 65
+    for i in range(1, x + 1):
+        spaces = " " * (x - i)
+        stars = "%c" %(a) * (2 * i -1)
+        print(spaces + stars)
+        a += 1
+
+x = int(input())
+
+pyramid(x)
+
+#=============================================================================================
+
+
+def pyramid(x):
+    
+    
+    for i in range(1, x + 1):
+        spaces = " " * (x - i)
+        stars = "*" * (2 * i -1)
+        print(spaces + stars)
+
+
+x = int(input())
+
+pyramid(x)
+
+
